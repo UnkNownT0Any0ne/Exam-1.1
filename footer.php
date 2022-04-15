32d06f7657d63170fdd776f3d417aa6b941440f1
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-04-15 12:40:17 +0200

    footer fix

--- footer.php
+++ footer.php
@@ -3,14 +3,9 @@
          <div class="footer__heading">
             <div class="footer__contacts">
                <div class="requisites">
-                  <p class="requisites__ogrn">1023900588876</p>
-                  <p class="requisites__inn">3904014901</p>
-                  <p class="requisites__bik">012748051</p>
-               </div>
-               <div class="contacts">
-                  <a href="tel:89003470173" class="phone">89003470173</a>
-                  <p class="contact__email">bigben39@edu.klgd.ru</p>
-                  <address class="contact__address">236044, Калининград, ул. Николая Карамзина, д. 6 (корпус1); 236029, ул. Л. Иванихиной, дом №9 (корпус 2)</address>
+                  <p class="requisites__ogrn">ОГРН: 1023900588876</p>
+                  <p class="requisites__inn">ИНН: 3904014901</p>
+                  <p class="requisites__bik">БИК: 012748051</p>
                </div>
                <div class="social-links">
                   <a href="#" class="social-links__item">instagram</a>
